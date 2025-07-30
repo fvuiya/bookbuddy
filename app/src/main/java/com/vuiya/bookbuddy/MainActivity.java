9b17549a7c2c1675358afa0192fdce609516f79a
Fahad Vuiya <projectultimatepredator@gmail.com>
2025-07-31 02:10:32 +0600

    Initial commit

---
+++ app/src/main/java/com/vuiya/bookbuddy/MainActivity.java
@@ -0,0 +1,66 @@
+package com.vuiya.bookbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.vuiya.bookbuddy.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CardView btnCamera, btnPdf;
+    private ImageButton btnLibrary, btnSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        setupClickListeners();
+    }
+
+    private void initViews() {
+        btnCamera = findViewById(R.id.btn_camera);
+        btnPdf = findViewById(R.id.btn_pdf);
+        btnLibrary = findViewById(R.id.btn_library);
+        btnSettings = findViewById(R.id.btn_settings);
+    }
+
+    private void setupClickListeners() {
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to Camera Activity
+                Toast.makeText(MainActivity.this, "Camera feature coming soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnPdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to PDF Selector Activity
+                Toast.makeText(MainActivity.this, "PDF feature coming soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Library feature coming soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Settings feature coming soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
